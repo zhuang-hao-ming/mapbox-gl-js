fdd3aebdfa008165c3a9ddbe14b6f1e767196e9f
Lucas Wojciechowski <lucas@mapbox.com>
2016-09-01 17:17:01 -0700

    Remove fake "canvas" module

--- js/util/canvas.js
+++ js/util/canvas.js
@@ -1,30 +1,43 @@
 'use strict';
 
-var gl = require('gl');
+var util = require('./util');
+var isSupported = require('mapbox-gl-supported');
 var window = require('./window');
 
 module.exports = Canvas;
 
-function Canvas(map) {
-    var requiredContextAttributes = {
-        antialias: false,
-        alpha: true,
-        stencil: true,
-        depth: true,
-        preserveDrawingBuffer: true
-    };
+function Canvas(map, container) {
+    this.canvas = window.document.createElement('canvas');
 
-    this.context = gl(
-        map.getContainer().offsetWidth * window.devicePixelRatio,
-        map.getContainer().offsetHeight * window.devicePixelRatio,
-        requiredContextAttributes
-    );
+    if (map && container) {
+        this.canvas.style.position = 'absolute';
+        this.canvas.classList.add('mapboxgl-canvas');
+        this.canvas.addEventListener('webglcontextlost', map._contextLost.bind(map), false);
+        this.canvas.addEventListener('webglcontextrestored', map._contextRestored.bind(map), false);
+        this.canvas.setAttribute('tabindex', 0);
+        container.appendChild(this.canvas);
+    }
 }
 
-Canvas.prototype.resize = function() {};
+Canvas.prototype.resize = function(width, height) {
+    var pixelRatio = window.devicePixelRatio || 1;
 
-Canvas.prototype.getWebGLContext = function() {
-    return this.context;
+    // Request the required canvas size taking the pixelratio into account.
+    this.canvas.width = pixelRatio * width;
+    this.canvas.height = pixelRatio * height;
+
+    // Maintain the same canvas size, potentially downscaling it for HiDPI displays
+    this.canvas.style.width = width + 'px';
+    this.canvas.style.height = height + 'px';
 };
 
-Canvas.prototype.getElement = function() {};
+Canvas.prototype.getWebGLContext = function(attributes) {
+    attributes = util.extend({}, attributes, isSupported.webGLContextAttributes);
+
+    return this.canvas.getContext('webgl', attributes) ||
+        this.canvas.getContext('experimental-webgl', attributes);
+};
+
+Canvas.prototype.getElement = function() {
+    return this.canvas;
+};
--- js/util/window.js
+++ js/util/window.js
@@ -1,6 +1,8 @@
 'use strict';
 
 var jsdom = require('jsdom');
+var util = require('./util');
+var gl = require('gl');
 
 var window = jsdom.jsdom().defaultView;
 
@@ -8,5 +10,21 @@ window.requestAnimationFrame = function(callback) { return setImmediate(callback
 window.cancelAnimationFrame = clearImmediate;
 
 window.devicePixelRatio = 1;
+
+window.HTMLCanvasElement.prototype.getContext = function(type, attributes) {
+    if (!this.context) {
+        var node = this;
+        while (node.offsetWidth == null) {
+            node = node.parentNode;
+        }
+
+        this.context = gl(
+            node.offsetWidth * window.devicePixelRatio,
+            node.offsetHeight * window.devicePixelRatio,
+            util.extend({}, attributes, {preserveDrawingBuffer: true})
+        );
+    }
+    return this.context;
+};
 
 module.exports = window;
