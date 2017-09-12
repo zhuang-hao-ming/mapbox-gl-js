18c2332193e6a12e74c6f9db54bdb0a257a62b4d
John Firebaugh <john.firebaugh@gmail.com>
2017-09-12 16:50:47 -0700

    New benchmarking system

---
+++ bench/benchmarks/layout_dds.js
@@ -0,0 +1,106 @@
+// @flow
+
+'use strict';
+
+const Layout = require('./layout');
+const TileCoord = require('../../src/source/tile_coord');
+
+const TILE_COUNT = 25;
+const LAYER_COUNT = 2;
+
+module.exports = class LayoutDDS extends Layout {
+    tileCoords(): Array<TileCoord> {
+        const coordinates = [];
+        const xRange = [9370, 9375];
+        const yRange = [12532, 12537];
+        while (coordinates.length < TILE_COUNT) {
+            for (let x = xRange[0]; x < xRange[1]; x++) {
+                for (let y = yRange[0]; y < yRange[1]; y++) {
+                    coordinates.push(new TileCoord(15, x, y));
+                }
+            }
+        }
+        return coordinates;
+    }
+
+    sourceID(): string {
+        return 'mapbox';
+    }
+
+    fetchStyle(): Promise<StyleSpecification> {
+        const style = {
+            "version": 8,
+            "sources": {
+                "mapbox": { "type": "vector", "url": "mapbox://mapbox.mapbox-streets-v7" }
+            },
+            "layers": []
+        };
+
+        const layers = [
+            {
+                "id": "road",
+                "type": "line",
+                "source": "mapbox",
+                "source-layer": "road",
+                "paint": {
+                    "line-width": 3,
+                    "line-color":{
+                        "type": "categorical",
+                        "property": "class",
+                        "stops":[
+                            [{"zoom": 0, "value": "motorway"}, "#0000FF"],
+                            [{"zoom": 0, "value": "trunk"}, "#000FF0"],
+                            [{"zoom": 0, "value": "primary"}, "#00FF00"],
+                            [{"zoom": 0, "value": "secondary"}, "#0FF000"],
+                            [{"zoom": 0, "value": "street"}, "#FF0000"],
+                            [{"zoom": 17, "value": "motorway"}, "#000088"],
+                            [{"zoom": 17, "value": "trunk"}, "#000880"],
+                            [{"zoom": 17, "value": "primary"}, "#008800"],
+                            [{"zoom": 17, "value": "secondary"}, "#088000"],
+                            [{"zoom": 17, "value": "street"}, "#880000"]
+                        ],
+                        "default": "#444444"
+                    }
+                }
+            },
+            {
+                "id": "poi",
+                "type": "circle",
+                "source": "mapbox",
+                "source-layer": "poi_label",
+                "paint": {
+                    "circle-radius": {
+                        "base": 2,
+                        "property": "scalerank",
+                        "stops":[
+                            [{"zoom": 0, "value": 0}, 1],
+                            [{"zoom": 0, "value": 10}, 5],
+                            [{"zoom": 17, "value": 0}, 20],
+                            [{"zoom": 17, "value": 10}, 50]
+                        ]
+                    },
+                    "circle-color": {
+                        "base": 1.25,
+                        "property": "localrank",
+                        "stops":[
+                            [{"zoom": 0, "value": 0}, "#002222"],
+                            [{"zoom": 0, "value": 10}, "#220022"],
+                            [{"zoom": 17, "value": 0}, "#008888"],
+                            [{"zoom": 17, "value": 10}, "#880088"]
+                        ]
+                    }
+                }
+            }
+        ];
+
+        while (style.layers.length < LAYER_COUNT) {
+            for (const layer of layers) {
+                style.layers.push(Object.assign({}, layer, {
+                    id: layer.id + style.layers.length
+                }));
+            }
+        }
+
+        return Promise.resolve(style);
+    }
+};
--- bench/benchmarks_view.js
+++ bench/benchmarks_view.js
@@ -1,233 +1,345 @@
 'use strict';
-/*eslint no-unused-vars: ["error", { "varsIgnorePattern": "BenchmarksView|clipboard" }]*/
 
-const Clipboard = require('clipboard');
+/* global d3 */
 
-// Benchmark results seem to be more consistent with a warmup and cooldown
-// period. These values are measured in milliseconds.
-const benchmarkCooldownTime = 250;
-const benchmarkWarmupTime  = 250;
+const versionColor = d3.scaleOrdinal(d3.schemeCategory10);
+versionColor(0); // Skip blue -- too similar to link color.
 
-const BenchmarksView = React.createClass({
+const formatSample = d3.format(".3r");
 
-    render: function() {
-        return <div style={{width: 960, paddingBottom: window.innerHeight, margin: '0 auto'}}>
-            <h1 className="space-bottom">
-                Benchmarks
-                <a
-                    className={[
-                        'fr',
-                        'icon',
-                        'clipboard',
-                        'button',
-                        (this.getStatus() === 'ended' ? '' : 'disabled')
-                    ].join(' ')}
-                    data-clipboard-text={this.renderTextBenchmarks()}>
-                    Copy Results
-                </a>
-            </h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Benchmark</th>
-                        {this.versions().map((v) => <th key={v}>{v}</th>)}
-                    </tr>
-                </thead>
-                <tbody>
-                    {Object.keys(this.state.results).map(this.renderBenchmark)}
-                </tbody>
-            </table>
-        </div>;
-    },
+class Plot extends React.Component {
+    render() {
+        return <svg width="100%" ref={node => this.node = node}></svg>;
+    }
 
-    renderBenchmark: function(name) {
-        return <tr key={name}>
-            <th><a href={`#${name}`} onClick={this.reload}>{name}</a></th>
-            {Object.keys(this.state.results[name]).map(this.renderBenchmarkVersion.bind(this, name))}
-        </tr>;
-    },
+    componentDidMount() {
+        this.plot();
+    }
 
-    renderBenchmarkVersion: function(name, version) {
-        const results = this.state.results[name][version];
-        const sampleData = results.samples ? results.samples.map(row => row.join(',')).join('\n') : null;
+    componentDidUpdate() {
+        this.plot();
+    }
+}
+
+class DensityPlot extends Plot {
+    plot() {
+        function kernelDensityEstimator(kernel, X) {
+            return function(V) {
+                // https://en.wikipedia.org/wiki/Kernel_density_estimation#A_rule-of-thumb_bandwidth_estimator
+                const bandwidth = 1.06 * d3.deviation(V) * Math.pow(V.length, -0.2);
+                return X.map(function(x) {
+                    return [x, d3.mean(V, function(v) { return kernel((x - v) / bandwidth); }) / bandwidth];
+                });
+            };
+        }
+
+        function kernelEpanechnikov(v) {
+            return Math.abs(v) <= 1 ? 0.75 * (1 - v * v) : 0;
+        }
+
+        const margin = {top: 20, right: 20, bottom: 30, left: 40},
+            width = this.node.clientWidth - margin.left - margin.right,
+            height = 200 - margin.top - margin.bottom;
+
+        const x = d3.scaleLinear()
+            .domain([0, d3.max(Array.prototype.concat.apply([], this.props.versions.map(version => version.samples)))])
+            .range([0, width])
+            .nice();
+
+        const density = kernelDensityEstimator(kernelEpanechnikov, x.ticks(50));
+        const versions = this.props.versions.map(version => ({
+            name: version.name,
+            density: version.samples.length ? density(version.samples) : undefined
+        }));
+        const yMax = d3.max(versions, version =>
+            d3.max(version.density || [], d => d[1]));
+
+        const y = d3.scaleLinear()
+            .domain([0, yMax])
+            .range([height, 0]);
+
+        const svg = d3.select(this.node)
+            .attr("height", height + margin.top + margin.bottom)
+            .selectAll("g")
+            .data([0]);
+
+        const enter = svg.enter()
+            .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        enter
+            .append("g")
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(x).tickFormat(formatSample))
+            .append("text")
+            .attr("fill", "#000")
+            .attr("x", width)
+            .attr("y", -6)
+            .style("text-anchor", "end")
+            .text("Time (ms)");
+
+        enter
+            .append("g")
+            .call(d3.axisLeft(y).ticks(4, "%"));
+
+        const version = svg.selectAll(".density")
+            .data(versions);
+
+        version.enter().append("path")
+            .attr("class", "density")
+            .attr("fill", "none")
+            .attr("stroke", version => versionColor(version.name))
+            .attr("stroke-opacity", 0.7)
+            .attr("stroke-width", 2)
+            .attr("stroke-linejoin", "round")
+            .merge(version)
+            .attr("d", version => version.density ? d3.line()
+                .curve(d3.curveBasis)
+                .x(d => x(d[0]))
+                .y(d => y(d[1]))(version.density) : "");
+    }
+}
+
+function regression(samples) {
+    const result = [];
+    for (let i = 0, n = 1; i + n < samples.length; i += n, n++) {
+        result.push([n, samples.slice(i, i + n).reduce(((sum, sample) => sum + sample), 0)]);
+    }
+    return result;
+}
+
+class RegressionPlot extends Plot {
+    plot() {
+        const margin = {top: 20, right: 20, bottom: 30, left: 40},
+            width = this.node.clientWidth - margin.left - margin.right,
+            height = 200 - margin.top - margin.bottom;
+
+        const versions = this.props.versions.filter(version => version.regression);
+
+        const x = d3.scaleLinear()
+            .domain([0, d3.max(versions.map(version => d3.max(version.regression.data, d => d[0])))])
+            .range([0, width])
+            .nice();
+
+        const y = d3.scaleLinear()
+            .domain([0, d3.max(versions.map(version => d3.max(version.regression.data, d => d[1])))])
+            .range([height, 0])
+            .nice();
+
+        const line = d3.line()
+            .x(d => x(d[0]))
+            .y(d => y(d[1]));
+
+        const svg = d3.select(this.node)
+            .attr("height", height + margin.top + margin.bottom)
+            .selectAll("g")
+            .data([0]);
+
+        const enter = svg.enter()
+            .append("g")
+            .attr("transform", `translate(${margin.left},${margin.top})`);
+
+        enter
+            .append("g")
+            .attr("class", "x axis")
+            .attr("transform", `translate(0,${height})`)
+            .call(d3.axisBottom(x))
+            .append("text")
+            .attr("fill", "#000")
+            .attr("x", width)
+            .attr("y", -6)
+            .style("text-anchor", "end")
+            .text("Iterations");
+
+        enter
+            .append("g")
+            .attr("class", "y axis")
+            .call(d3.axisLeft(y).ticks(4).tickFormat(formatSample))
+            .append("text")
+            .attr("fill", "#000")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Time (ms)");
+
+        let group = svg.selectAll(".version")
+            .data(versions);
+
+        group = group.enter().append("g")
+            .attr("class", "version")
+            .style("fill", version => versionColor(version.name))
+            .style("fill-opacity", 0.7)
+            .merge(group);
+
+        let regressionPoints = group.selectAll("circle")
+            .data(version => version.regression.data);
+
+        regressionPoints = regressionPoints.enter().append("circle")
+            .attr("r", 2)
+            .merge(regressionPoints);
+
+        regressionPoints
+            .attr("cx", d => x(d[0]))
+            .attr("cy", d => y(d[1]));
+
+        let regressionLine = group.selectAll('.regression-line')
+            .data((version) => [version]);
+
+        regressionLine = regressionLine.enter().append('path')
+            .attr('class', 'regression-line')
+            .attr('stroke', version => versionColor(version.name))
+            .attr('stroke-width', 1)
+            .attr('stroke-opacity', 0.5)
+            .merge(regressionLine);
+
+        regressionLine.attr('d', version => line(version.regression.data.map(d => [
+            d[0],
+            d[0] * version.regression.slope + version.regression.intercept
+        ])));
+    }
+}
+
+class BenchmarkStatistic extends React.Component {
+    render() {
+        switch (this.props.status) {
+            case 'waiting':
+                return <p className="quiet"></p>;
+            case 'running':
+                return <p>Running...</p>;
+            case 'error':
+                return <p>{this.props.error.message}</p>;
+            default:
+                return <p>{this.props.statistic(this.props)}</p>;
+        }
+    }
+}
+
+class BenchmarkRow extends React.Component {
+    render() {
+        const ended = this.props.versions.find(version => version.status === 'ended');
         return (
-            <td id={name + version}
-                key={version}
-                className={results.status === 'waiting' ? 'quiet' : ''}>
-                {results.logs.map((log, index) => {
-                    return <div key={index} className={`pad1 dark fill-${log.color}`}>{log.message}</div>;
-                })}
-                {sampleData ? (
-                    <details>
-                        <summary className='pad1 dark fill-green'>
-                            Sample Data
-                            <a className='icon clipboard'
-                                data-clipboard-text={sampleData}>Copy</a>
-                        </summary>
-                        <pre>{sampleData}</pre>
-                    </details>
-                ) : ''}
-            </td>
+            <div className="col12 clearfix space-bottom">
+                <h2 className="col4"><a href={`#${this.props.name}`} onClick={this.reload}>{this.props.name}</a></h2>
+                <div className="col8">
+                    <table className="fixed">
+                        <tr><th></th>{this.props.versions.map(version => <th style={{color: versionColor(version.name)}} key={version.name}>{version.name}</th>)}</tr>
+                        {this.renderStatistic('R² Slope / Correlation',
+                            (version) => `${formatSample(version.regression.slope)} ms / ${version.regression.correlation.toFixed(3)} ${
+                                version.regression.correlation < 0.9 ? '\u2620\uFE0F' :
+                                version.regression.correlation < 0.99 ? '\u26A0\uFE0F' : ''}`)}
+                        {this.renderStatistic('Mean',
+                            (version) => `${formatSample(d3.mean(version.samples))} ms`)}
+                        {this.renderStatistic('Minimum',
+                            (version) => `${formatSample(d3.min(version.samples))} ms`)}
+                        {this.renderStatistic('Variance',
+                            (version) => `${formatSample(d3.variance(version.samples))} ms`)}
+                        {this.renderStatistic('Deviation',
+                            (version) => `${formatSample(d3.deviation(version.samples))} ms`)}
+                    </table>
+                    {ended && <DensityPlot versions={this.props.versions}/>}
+                    {ended && <RegressionPlot versions={this.props.versions}/>}
+                </div>
+            </div>
         );
-    },
+    }
 
-    versions: function() {
-        const versions = [];
-        for (const name in this.state.results) {
-            for (const version in this.state.results[name]) {
-                if (versions.indexOf(version) < 0) {
-                    versions.push(version);
-                }
-            }
-        }
-        return versions;
-    },
-
-    renderTextBenchmarks: function() {
-        const versions = this.versions();
-        let output = `benchmark | ${versions.join(' | ')}\n---`;
-        for (let i = 0; i < versions.length; i++) {
-            output += ' | ---';
-        }
-        output += '\n';
-
-        for (const name in this.state.results) {
-            output += `**${name}**`;
-            for (const version of versions) {
-                const result = this.state.results[name][version];
-                output += ` | ${result && result.message || 'n\/a'} `;
-            }
-            output += '\n';
-        }
-        return output;
-    },
-
-    getInitialState: function() {
-        const results = {};
-
-        for (const name in this.props.benchmarks) {
-            for (const version in this.props.benchmarks[name]) {
-                if (!this.props.benchmarkFilter || this.props.benchmarkFilter(name, version)) {
-                    results[name] = results[name] || {};
-                    results[name][version] = {
-                        status: 'waiting',
-                        logs: []
-                    };
-                }
-            }
-        }
-
-        return { results: results };
-    },
-
-    componentDidMount: function() {
-        const that = this;
-
-        asyncSeries(Object.keys(that.state.results), (name, callback) => {
-            asyncSeries(Object.keys(that.state.results[name]), (version, callback) => {
-                that.runBenchmark(name, version, callback);
-            }, callback);
-        }, (err) => {
-            if (err) throw err;
-        });
-    },
-
-    runBenchmark: function(name, version, outerCallback) {
-        const that = this;
-        const results = this.state.results[name][version];
-
-        function log(color, message) {
-            results.logs.push({
-                color: color || 'blue',
-                message: message
-            });
-            that.forceUpdate();
-        }
-
-        function callback() {
-            setTimeout(outerCallback, benchmarkCooldownTime);
-        }
-
-        results.status = 'running';
-        log('dark', 'starting');
-
-        setTimeout(() => {
-            const emitter = that.props.benchmarks[name][version]();
-
-            emitter.on('log', (event) => {
-                log(event.color, event.message);
-
-            });
-
-            emitter.on('end', (event) => {
-                results.message = event.message;
-                results.status = 'ended';
-                results.samples = event.samples;
-                log('green', event.message);
-                callback();
-
-            });
-
-            emitter.on('error', (event) => {
-                results.status = 'errored';
-                log('red', event.error);
-                callback();
-            });
-
-        }, benchmarkWarmupTime);
-    },
-
-    getBenchmarkVersionStatus: function(name, version) {
-        return this.state.results[name][version].status;
-    },
-
-    getBenchmarkStatus: function(name) {
-        return reduceStatuses(Object.keys(this.state.results[name]).map(function(version) {
-            return this.getBenchmarkVersionStatus(name, version);
-        }, this));
-    },
-
-    getStatus() {
-        return reduceStatuses(Object.keys(this.state.results).map(function(name) {
-            return this.getBenchmarkStatus(name);
-        }, this));
-    },
+    renderStatistic(title, statistic) {
+        return (
+            <tr>
+                <th>{title}</th>
+                {this.props.versions.map(version =>
+                    <td key={version.name}><BenchmarkStatistic statistic={statistic} {...version}/></td>
+                )}
+            </tr>
+        );
+    }
 
     reload() {
         location.reload();
     }
-});
+}
 
-function reduceStatuses(statuses) {
-    if (statuses.indexOf('running') !== -1) {
-        return 'running';
-    } else if (statuses.indexOf('waiting') !== -1) {
-        return 'waiting';
-    } else {
-        return 'ended';
+class BenchmarksTable extends React.Component {
+    render() {
+        return (
+            <div style={{width: 960, margin: '2em auto'}}>
+                <h1 className="space-bottom1">Mapbox GL JS Benchmarks</h1>
+                {this.props.benchmarks.map(benchmark => <BenchmarkRow key={benchmark.name} {...benchmark}/>)}
+            </div>
+        );
     }
 }
 
-const clipboard = new Clipboard('.clipboard');
+const versions = window.mapboxglVersions;
+const benchmarks = [];
+const filter = window.location.hash.substr(1);
 
-ReactDOM.render(
-    <BenchmarksView
-        benchmarks={window.mapboxglBenchmarks}
-        benchmarkFilter={function(name) {
-            const nameFilter = window.location.hash.substr(1);
-            return !nameFilter || name === nameFilter;
-        }}
-    />,
-    document.getElementById('benchmarks')
-);
+let finished = false;
+let promise = Promise.resolve();
 
-function asyncSeries(array, iterator, callback) {
-    if (array.length) {
-        iterator(array[0], (err) => {
-            if (err) callback(err);
-            else asyncSeries(array.slice(1), iterator, callback);
+for (const name in window.mapboxglBenchmarks) {
+    if (filter && name !== filter)
+        continue;
+
+    const benchmark = { name, versions: [] };
+    benchmarks.push(benchmark);
+
+    for (const ver in window.mapboxglBenchmarks[name]) {
+        const version = {
+            name: ver,
+            status: 'waiting',
+            logs: [],
+            samples: []
+        };
+
+        benchmark.versions.push(version);
+
+        promise = promise.then(() => {
+            version.status = 'running';
+            update();
+
+            return window.mapboxglBenchmarks[name][ver].run()
+                .then(samples => {
+                    version.status = 'ended';
+                    version.samples = samples;
+                    version.regression = leastSquaresRegression(regression(samples));
+                    update();
+                })
+                .catch(error => {
+                    version.status = 'errored';
+                    version.error = error;
+                    update();
+                });
         });
-    } else {
-        callback();
     }
+}
+
+promise = promise.then(() => {
+    finished = true;
+    update();
+})
+
+function update() {
+    ReactDOM.render(
+        <BenchmarksTable versions={versions} benchmarks={benchmarks} finished={finished}/>,
+        document.getElementById('benchmarks')
+    );
+}
+
+function leastSquaresRegression(data) {
+    const meanX = d3.sum(data, d => d[0]) / data.length;
+    const meanY = d3.sum(data, d => d[1]) / data.length;
+    const varianceX = d3.variance(data, d => d[0]);
+    const sdX = Math.sqrt(varianceX);
+    const sdY = d3.deviation(data, d => d[1]);
+    const covariance = d3.sum(data, ([x, y]) =>
+        (x - meanX) * (y - meanY)
+    ) / (data.length - 1);
+
+    const correlation = covariance / sdX / sdY;
+    const slope = covariance / varianceX;
+    const intercept = meanY - slope * meanX;
+
+    return { correlation, slope, intercept, data };
 }
