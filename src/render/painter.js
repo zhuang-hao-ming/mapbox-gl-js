c5b0808b9826d3ffd76a999619d14720aa655084
Molly Lloyd <molly@mapbox.com>
2017-09-21 16:31:35 -0700

    use separate renderpass to render terrain prepare textures

--- src/render/painter.js
+++ src/render/painter.js
@@ -38,7 +38,7 @@ import type LineAtlas from './line_atlas';
 import type ImageManager from './image_manager';
 import type GlyphManager from './glyph_manager';
 
-export type RenderPass = '3d' | 'opaque' | 'translucent';
+export type RenderPass = '3d' | 'hillshadeprepare' | 'opaque' | 'translucent';
 
 type PainterOptions = {
     showOverdrawInspector: boolean,
@@ -343,6 +343,36 @@ class Painter {
                 renderTarget.unbind();
             }
         }
+
+
+
+        this.renderPass = 'hillshadeprepare';
+
+        {
+            let sourceCache;
+            let coords = [];
+
+            for (let i = 0; i < layerIds.length; i++) {
+                const layer = this.style._layers[layerIds[i]];
+
+                if (layer.type !== 'hillshade' || layer.isHidden(this.transform.zoom)) continue;
+
+                if (layer.source !== (sourceCache && sourceCache.id)) {
+                    sourceCache = this.style.sourceCaches[layer.source];
+                    coords = [];
+
+                    if (sourceCache) {
+                        this.clearStencil();
+                        coords = sourceCache.getVisibleCoordinates();
+                    }
+
+                    coords.reverse();
+                }
+
+                this.renderLayer(this, (sourceCache: any), layer, coords);
+            }
+        }
+
 
         // Clear buffers in preparation for drawing to the main framebuffer
         this.clearColor();
