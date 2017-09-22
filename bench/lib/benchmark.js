ab45bfeb4e3b4cb3203df04065c3774d44d2a4e9
John Firebaugh <john.firebaugh@gmail.com>
2017-09-22 10:22:47 -0700

    Avoid Promise overhead for sync benchmarks

--- bench/lib/benchmark.js
+++ bench/lib/benchmark.js
@@ -3,6 +3,10 @@
 'use strict';
 
 class Benchmark {
+    constructor() {
+        this._async = this._async.bind(this);
+    }
+
     /**
      * The `setup` method is intended to be overridden by subclasses. It will be called once, prior to
      * running any benchmark iterations, and may set state on `this` which the benchmark later accesses.
@@ -25,29 +29,62 @@ class Benchmark {
      */
     teardown(): Promise<void> | void {}
 
+    _async: () => Promise<Array<number>>;
+    _elapsed: number;
+    _samples: Array<number>;
+    _start: number;
+
     /**
      * Run the benchmark by executing `setup` once, sampling the execution time of `bench` some number of
      * times, and then executing `teardown`. Yields an array of execution times.
      */
     run(): Promise<Array<number>> {
-        let elapsed = 0;
-        const samples = [];
+        return Promise.resolve(this.setup()).then(() => this._begin());
+    }
 
-        const next = () => {
-            if (elapsed >= 5000) {
-                return Promise.resolve(this.teardown()).then(() => samples);
+    _begin(): Promise<Array<number>> {
+        this._samples = [];
+        this._elapsed = 0;
+        this._start = performance.now();
+
+        const bench = this.bench();
+        if (bench instanceof Promise) {
+            return bench.then(this._async);
+        } else {
+            return this._sync();
+        }
+    }
+
+    _sync(): Promise<Array<number>> {
+        // Avoid Promise overhead for sync benchmarks.
+        while (true) {
+            const sample = performance.now() - this._start;
+            this._samples.push(sample);
+            this._elapsed += sample;
+            if (this._elapsed >= 1000) {
+                return this._end();
             }
+            this._start = performance.now();
+            this.bench();
+        }
 
-            const start = performance.now();
-            return Promise.resolve(this.bench()).then(() => {
-                const sample = performance.now() - start;
-                elapsed += sample;
-                samples.push(sample);
-                return next();
-            });
-        };
+        // Unreachable, but placate flow.
+        return this._end();
+    }
 
-        return Promise.resolve(this.setup()).then(next);
+    _async(): Promise<Array<number>> {
+        const sample = performance.now() - this._start;
+        this._samples.push(sample);
+        this._elapsed += sample;
+        if (this._elapsed >= 1000) {
+            return this._end();
+        }
+        this._start = performance.now();
+        return ((this.bench(): any): Promise<void>).then(this._async);
+    }
+
+    _end(): Promise<Array<number>> {
+        return Promise.resolve(this.teardown()).then(() => this._samples);
     }
 }
 
